5a2e4497d964a31eabd2726a0789df6346742422
靳少辉 <1051131737@qq.com>
2021-07-29 17:57:37 +0800

    配置文件添加注释

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,22 +41,28 @@ module.exports = {
     selectText: '选择语言', // 多语言下拉菜单的标题
     label: '简体中文', // 该语言在下拉菜单中的标签
     editLinkText: '在 GitHub 上编辑此页', // 编辑链接文字
-    // Service Worker 的配置
+    docsDir: 'src',
+    /** Service Worker 的配置 **/
     serviceWorker: {
       updatePopup: {
         message: "发现新内容可用.",
         buttonText: "刷新"
       }
     },
+    /** 搜索配置 **/
     search: true,
     searchMaxSuggestions: 10, // 搜索信息最大数量
     algolia: {}, // 当前 locale 的 algolia docsearch 选项
+    /**  **/
     displayAllHeaders: true, // 默认值：false
-    lastUpdated: '最近更新时间', // 文件最后更新时间 string = 'Last Updated' | boolean 若为字符串则拼接在前
-    navbar: true, // 禁用导航栏，单页面禁用 则需要在相应md YAML front matter 配置 navbar: false
+    /** 文件最后更新时间 string = 'Last Updated' | boolean 若为字符串则拼接在前  **/
+    lastUpdated: '最近更新时间',
+    /** 菜单配置，单页面禁用 则需要在相应md YAML front matter 配置 navbar: false **/
+    navbar: true,
     nav: nav,
     sidebar: sidebar
   },
+  /** 插件配置 **/
   plugins: [
     [
       'vuepress-plugin-comment',
