cf3082340747b5439016353000dc2c9d98144344
靳少辉 <1051131737@qq.com>
2022-05-30 16:16:28 +0800

    尝试修复分类导致里外图片显示不一样问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ const { homeSidebarB } = require('./ads/index')
 module.exports = (options, context) => ({
   title: '繁华中自律，落魄中自愈', // 网站的标题
   description: 'Just playing around', // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中
-  base: '/', // base 将会作为前缀自动地插入到所有以 / 开始的其他选项的链接中
+  base: './', // base 将会作为前缀自动地插入到所有以 / 开始的其他选项的链接中
   // host: '10.0.3.148', // 用于 dev server 的主机名
   // host: '192.168.31.160', // 用于 dev server 的主机名
   host: '127.0.0.1', // 用于 dev server 的主机名
