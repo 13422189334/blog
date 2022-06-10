d155cb321ca946a49c91230edf5185531704341c
靳少辉 <1051131737@qq.com>
2022-06-10 12:00:54 +0800

    部分路径修改，打包更新代码块功能

--- docs/.vuepress/router/index.js
+++ docs/.vuepress/router/index.js
@@ -3,7 +3,7 @@ const nav  = [
   { text: '前端知识库', link: '/FrontEnd/' },
   { text: '后端知识库', link: '/BackEnd/' },
   { text: '设计模式', link: '/DesignPattern/' },
-  { text: '常用算法', link: '/Algorithm/Dichotomy' },
+  { text: '常用算法', link: '/Algorithm/Dichotomy/' },
   { text: 'Git', link: '/Git/' },
   { text: '服务器', link: '/Server/' },
   { text: '性能优化', link: '/Performance/' },
@@ -12,8 +12,8 @@ const nav  = [
 
 const sidebar = {
   '/Algorithm/': [
-    'Dichotomy', // 二分查找理论
-    'DynamicProgram' // 动态规划算法
+    'Dichotomy/', // 二分查找理论
+    'DynamicProgram/' // 动态规划算法
   ]
 }
 
---
+++ docs/Algorithm/DynamicProgram/index.md
@@ -0,0 +1,13 @@
+---
+title: 动态规划算法
+date: 2022-06-06 16:38:19
+permalink: /Algorithm/DynamicProgram/
+sidebar: true # 不显示侧边栏
+article: true # 不是文章页 (不显示面包屑栏、最近更新栏等)
+comment: true # 不显示评论栏
+editLink: true # 不显示编辑按钮
+categories:
+  - Algorithm
+tags:
+  - 很菜的算法
+---
